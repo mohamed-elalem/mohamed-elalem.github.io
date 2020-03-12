bde3ce804b2ad4c53cbd3a818751d77d272b7545
Mohamed Elalem <mohamed.el.alem.2017@gmail.com>
2020-03-12 14:27:47 -0500

    add mouse maze and jquery quiz to index page

---
+++ assets/javascript/mouse-maze.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+  let game_began = false;
+
+  const restart = function() {
+    game_began = false;
+    $('.boundary').css({backgroundColor: '#eeeeee'});
+  }
+
+  $('#start').click(() => {
+    game_began = true;
+  });
+
+  $('.boundary').mouseenter(() => {
+    if (game_began === false) {
+      return;
+    }
+    
+    $('.boundary').css({backgroundColor: 'red'});
+    setTimeout(() => {
+      alert('You lost the game');
+      restart();
+    }, 200);
+  });
+
+  $('#end').mouseenter(() => {
+    if (game_began === false) {
+      return;
+    }
+
+    $('.boundary').css({backgroundColor: 'green'});
+    setTimeout(() => {
+      alert('You won the game');
+      restart();
+    }, 200);
+  });
+});
